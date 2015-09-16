63d46bc3a1864bfc79b05861bb59a6b378b24446
DaRochaRomain <darocha.romain@gmail.com>
2015-09-16 18:27:02 +0200

    Update Install.sh

--- C++/Install.sh
+++ C++/Install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+#Uninstall any previous libopenblas
+sudo apt-get remove libopenblas-dev
+sudo apt-get remove libopenblas-base
+
 #Install libopenblas et liblapack
-sudo apt-get install libopenblas-base
-sudo apt-get install libopenblas-dev
 sudo apt-get install liblapack-dev
+
+#Build and install OpenBlas
+rm -rf OpenBLAS
+git clone git://github.com/xianyi/OpenBLAS
+cd OpenBLAS
+make
+sudo make install
+cd..
+rm -rf OpenBLAS
 
 #Build and install Armadillo
 cd Armadillo
