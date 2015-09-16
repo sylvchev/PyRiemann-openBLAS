3671c10c39b4f3d1280b67b3e1626f29a8726396
DaRochaRomain <darocha.romain@gmail.com>
2015-09-16 18:55:06 +0200

    Update Install.sh

--- C++/Install.sh
+++ C++/Install.sh
@@ -6,7 +6,7 @@ sudo apt-get install libopenblas-dev
 sudo apt-get install liblapack-dev
 sudo apt-get install libarpack-dev
 sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get upgrade
 
 #Build and install Armadillo
 cd Armadillo
