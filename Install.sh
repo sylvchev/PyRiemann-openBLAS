4ab6b310b4acc35236b99521397b2890e25e19f0
DA ROCHA Romain <darocha.romain@gmail.com>
2015-10-03 16:15:05 +0200

    compilation manuelle de openblas

--- Install.sh
+++ Install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-#Install librairies
-sudo apt-get install libopenblas-dev liblapack-dev python3 python3-numpy python3-scipy
+
+#install and remove package
+sudo apt-get remove libopenblas* liblapack*
+sudo apt-get install python3 python3-dev python3-numpy python3-scipy cython3 gcc gfortran
+
+
+#install OpenBLAS
+git clone git://github.com/xianyi/OpenBLAS
+cd OpenBLAS
+make
+sudo make install
+cd ..
+sudo rm -rf OpenBLAS
+export BLAS=/opt/OpenBLAS/lib/libopenblas.so
+export LAPACK=/opt/OpenBLAS/lib/libopenblas.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/OpenBLAS/lib
+
+
+#permanant export
+echo -e "export BLAS=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
+echo -e "export LAPACK=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
+echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/OpenBLAS/lib" >> ~/.bashrc
