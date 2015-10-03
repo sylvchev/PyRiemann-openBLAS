32d6f61f9c96f633bcbd7a96e79102bbb8ca3e68
DA ROCHA Romain <darocha.romain@gmail.com>
2015-10-03 17:39:11 +0200

    finalisation du script d'installation avec la derniere version de openblas telecharge, compile et install openblas telecharge et compile numpy scipy avec pip
    fonctionne seulement avec python3

--- Install.sh
+++ Install.sh
@@ -5,21 +5,23 @@
 sudo apt-get install gcc g++ gfortran
 git clone git://github.com/xianyi/OpenBLAS
 cd OpenBLAS
-make
-sudo make install
+make FC=gfortran
+sudo make install PREFIX=/usr/local
 cd ..
 sudo rm -rf OpenBLAS
 
 
+#export and permanant export
+export BLAS=/usr/local/lib/libopenblas.so
+export LAPACK=/usr/local/lib/libopenblas.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+echo -e "export BLAS=/usr/local/lib/libopenblas.so" >> ~/.bashrc
+echo -e "export LAPACK=/usr/local/lib/libopenblas.so" >> ~/.bashrc
+echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
+
+
 #install and remove package
 sudo apt-get remove libopenblas* liblapack*
-sudo apt-get install python3 python3-numpy python3-scipy 
-
-
-#export and permanant export
-export BLAS=/opt/OpenBLAS/lib/libopenblas.so
-export LAPACK=/opt/OpenBLAS/lib/libopenblas.so
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/OpenBLAS/lib
-echo -e "export BLAS=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
-echo -e "export LAPACK=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
-echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/OpenBLAS/lib" >> ~/.bashrc
+sudo apt-get install python3 python3-dev python3-pip
+sudo pip3 uninstall numpy scipy
+sudo pip3 install numpy scipy
