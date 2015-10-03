159224665f3c0e2577fdb8560a924a5ff55fd2fa
DA ROCHA Romain <darocha.romain@gmail.com>
2015-10-03 16:26:35 +0200

    gawgawgag

--- Install.sh
+++ Install.sh
@@ -9,9 +9,6 @@ make
 sudo make install
 cd ..
 sudo rm -rf OpenBLAS
-export BLAS=/opt/OpenBLAS/lib/libopenblas.so
-export LAPACK=/opt/OpenBLAS/lib/libopenblas.so
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/OpenBLAS/lib
 
 
 #install and remove package
@@ -19,7 +16,10 @@ sudo apt-get remove libopenblas* liblapack*
 sudo apt-get install python3 python3-numpy python3-scipy 
 
 
-#permanant export
+#export and permanant export
+export BLAS=/opt/OpenBLAS/lib/libopenblas.so
+export LAPACK=/opt/OpenBLAS/lib/libopenblas.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/OpenBLAS/lib
 echo -e "export BLAS=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
 echo -e "export LAPACK=/opt/OpenBLAS/lib/libopenblas.so" >> ~/.bashrc
 echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/opt/OpenBLAS/lib" >> ~/.bashrc
