9e074e8bb3676ce0a18bdde7eebb69aedc29471f
DA ROCHA Romain <darocha.romain@gmail.com>
2015-10-04 02:31:11 +0200

    Modification du script d'installation pour le rendre totalement silencieux et fonctionnel

--- Install.sh
+++ Install.sh
@@ -4,13 +4,15 @@
 #install OpenBLAS
 echo -e "Installing prerequisite packages for OpenBLAS : gcc & gfortran"
 sudo apt-get -y install gcc gfortran 2>&1 >/dev/null
+export CC="gcc -w"
+export FC="gfortran -w"
 
 echo -e "Downloading OpenBLAS : cloning from Github"
 git clone -q git://github.com/xianyi/OpenBLAS
 
 echo -e "Building OpenBLAS, please wait (this can take several minutes)"
 cd OpenBLAS
-make -s FC=gfortran 2>&1 >/dev/null
+make -s 2>&1 >/dev/null
 
 echo -e "Installing OpenBLAS at /usr/local"
 sudo make install PREFIX=/usr/local 2>&1 >/dev/null
@@ -33,12 +35,15 @@ echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 echo -e "Removing current libopenblas and liblapack packages"
 sudo apt-get -y remove libopenblas* liblapack* 2>&1 >/dev/null
 
-echo -e "Installing python prerequisite packages : python3 python3-dev python3-pip"
-sudo apt-get -y install python3 python3-dev python3-pip 2>&1 >/dev/null
+echo -e "Installing python prerequisite packages : python3 python3-dev python3-pip g++"
+sudo apt-get -y install python3 python3-dev python3-pip g++ 2>&1 >/dev/null
 
 echo -e "Removing current numpy and scipy packages"
 sudo apt-get -y remove python3-numpy python3-scipy 2>&1 >/dev/null
 yes | sudo pip3 uninstall -q numpy scipy 2>&1 >/dev/null
 
-echo -e "Compiling and installing numpy and scipy packages"
+echo -e "Downloading, compiling and installing numpy package (this can take several minutes)"
+yes | sudo pip3 install -q numpy scipy 2>&1 >/dev/null
+
+echo -e "Downloading, compiling and installing scipy package (this can take several minutes)"
 yes | sudo pip3 install -q numpy scipy 2>&1 >/dev/null
