d0faed1d7f5951332c6217a7892708832ef57395
DaRochaRomain <darocha.romain@gmail.com>
2015-10-05 15:34:58 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,6 +2,9 @@
 
 
 #install OpenBLAS
+echo -e "Removing current libopenblas and liblapack packages"
+sudo apt-get -y remove libopenblas* liblapack* 2>&1 >/dev/null
+
 echo -e "Installing prerequisite packages for OpenBLAS : gcc & gfortran"
 sudo apt-get -y install gcc gfortran 2>&1 >/dev/null
 export CC="gcc -w"
@@ -22,19 +25,7 @@ cd ..
 sudo rm -rf OpenBLAS
 
 
-#export and permanant export
-export BLAS=/usr/local/lib/libopenblas.so
-export LAPACK=/usr/local/lib/libopenblas.so
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-echo -e "export BLAS=/usr/local/lib/libopenblas.so" >> ~/.bashrc
-echo -e "export LAPACK=/usr/local/lib/libopenblas.so" >> ~/.bashrc
-echo -e "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
-
-
-#install and remove package
-echo -e "Removing current libopenblas and liblapack packages"
-sudo apt-get -y remove libopenblas* liblapack* 2>&1 >/dev/null
-
+#install python numpy and scipy
 echo -e "Installing python prerequisite packages : python3 python3-dev python3-pip g++"
 sudo apt-get -y install python3 python3-dev python3-pip g++ 2>&1 >/dev/null
 
@@ -43,7 +34,7 @@ sudo apt-get -y remove python3-numpy python3-scipy 2>&1 >/dev/null
 yes | sudo pip3 uninstall -q numpy scipy 2>&1 >/dev/null
 
 echo -e "Downloading, compiling and installing numpy package (this can take several minutes)"
-yes | sudo pip3 install -q numpy scipy 2>&1 >/dev/null
+yes | sudo pip3 install -q numpy 2>&1 >/dev/null
 
 echo -e "Downloading, compiling and installing scipy package (this can take several minutes)"
-yes | sudo pip3 install -q numpy scipy 2>&1 >/dev/null
+yes | sudo pip3 install -q scipy 2>&1 >/dev/null
