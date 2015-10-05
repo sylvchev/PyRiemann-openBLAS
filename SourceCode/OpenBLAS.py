72b6c4c6efcc2c37483c1224982cbd4bbc614b11
DaRochaRomain <darocha.romain@gmail.com>
2015-10-05 17:52:44 +0200

    Update OpenBLAS.py

--- SourceCode/OpenBLAS.py
+++ SourceCode/OpenBLAS.py
@@ -11,18 +11,7 @@ import Environment
 import ctypes
 from ctypes.util import find_library
 
-# enforces priority of hand-compiled OpenBLAS library over version in /usr/lib
-# that comes from Ubuntu repos
-try_paths = ['/opt/OpenBLAS/lib/libopenblas.so', '/lib/libopenblas.so', '/usr/lib/libopenblas.so.0', find_library('openblas')]
-openblas_lib = None
-for libpath in try_paths :
-	try :
-		openblas_lib = ctypes.cdll.LoadLibrary(libpath)
-		break
-	except OSError:
-		continue
-if openblas_lib is None :
-	raise EnvironmentError('Could not locate an OpenBLAS shared library', 2)
+openblas_lib = ctypes.cdll.LoadLibrary('/usr/lib/libopenblas.so')
 
 
 
@@ -41,18 +30,17 @@ def GetNbProc():
 
 
 
-def NbThreads(n):
-	return ThreadContext(int(n)) 
-
-
-
-class ThreadContext(object):
+class NbThreads :
 	def __init__(self, nbThreads):
 		self.oldNbTreads = GetNbThreads()
 		self.nbThreads = nbThreads
 
+
+
 	def __enter__(self):
 		SetNbThreads(self.nbThreads)
+
+
 
 	def __exit__(self, *args):
 		SetNbThreads(self.oldNbTreads)
