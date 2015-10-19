059213e3e20316a4284fa44a85e7eca6fb3c085d
DA ROCHA Romain <darocha.romain@gmail.com>
2015-10-19 11:49:45 +0200

    passage sur fichier de configuration

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PyRiemann
-Python implementation for computation in Riemannian geometry, using OpenBLAS and LAPACK
+Python implementation for computation in Riemannian geometry, using OpenBLAS
 Based on : https://github.com/alexandrebarachant/pyRiemann
--- Utils/Environment.py
+++ Utils/Environment.py
@@ -8,29 +8,43 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 
 import Utils.OpenBLAS as OpenBLAS
 
-# Used to set data type of matrix. Use only :
-# 	- 'd' for double precision
-# 	- 'f' for single precision, algorithms should be a little faster (≈ 10%)
-data_type = numpy.dtype('d')
-
-# Used to determine if data is copied when the CovMat class is created from an array or an existing matrix,
-# or if the data is shared
-# Used to avoid the memory allocation, and copy cost
-memory_safe_state = False
-
-# Number of thread used by those algorithm
-# change by :
-# nbThreads = X
-# where X is the number of threads you want to use, or None if you want to use all disponible threads
-# avoid hyperthreading, most of the time, alorithms are slower with it
-nb_threads = 2
-
-
 # ---------------------------------------------------------------------------- #
 # ------------------------------- DO NOT TOUCH ------------------------------- #
 # ---------------------------------------------------------------------------- #
 
-if nb_threads is not None:
-    OpenBLAS.set_nb_threads(nb_threads)
-else:
-    OpenBLAS.set_nb_threads(OpenBLAS.get_nb_procs())
+data_type = None
+memory_safe_state = None
+nb_threads = None
+
+with open("../config.cfg") as file:
+    for line in file.readlines():
+            words = line.split('=')
+
+            if words[0] == "data_type":
+                if words[1] == "double\n":
+                    data_type = numpy.dtype('d')
+                elif words[1] == "float\n":
+                    data_type = numpy.dtype('f')
+                elif words[1] == "integer\n":
+                    data_type = numpy.dtype('i')
+                else:
+                    print("Error in data type while parsing config.cfg. Data type automatically set to double precision")
+                    data_type = numpy.dtype('d')
+
+            if words[0] == "memory_safe_state":
+                if words[1] == "True\n" or words[1] == "true\n":
+                    memory_safe_state = True
+                elif words[1] == "False\n" or words[1] == "false\n":
+                    memory_safe_state = True
+                else:
+                    print("Error in memory safe state while parsing config.cfg. memory safe state automatically set to True")
+                    memory_safe_state = True
+
+            if words[0] == "nb_threads":
+                try:
+                    nb_threads = int(words[1])
+                except ValueError:
+                    print("Error in number of threads while parsing config.cfg. Number of threads automatically set to the max possible")
+                    nb_threads = OpenBLAS.get_nb_procs()
+
+OpenBLAS.set_nb_threads(nb_threads)
