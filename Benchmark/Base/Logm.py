9b5adece89f1a41a93b720bcf5868717e296bc33
DA ROCHA Romain <darocha.romain@gmail.com>
2015-12-04 10:35:43 +0100

    modif benchmark

--- Benchmark/Base/Logm.py
+++ Benchmark/Base/Logm.py
@@ -17,14 +17,14 @@ for i in range(0, 10):
 
 for i in range(0, len(size)):
     A = numpy.random.rand(size[i], 2 * size[i])
-    covmat = numpy.dot(A, A.T)
+    covmat = numpy.dot(A, A.T) / 1000
     t = timeit.Timer("logm(covmat)",
                      setup="from __main__ import covmat; from oldPyRiemann.base import logm; import Utils.OpenBLAS")
-    old_time = t.timeit(number=int(100000 / size[i])) / int(100000 / size[i])
+    old_time = t.timeit(number=int(1e9 / size[i] ** 2)) / int(1e9 / size[i] ** 2)
 
     covmat = CovMat.random(size[i])
     t = timeit.Timer("covmat.reset_fields(); covmat.logm", setup="from __main__ import covmat")
-    new_time = t.timeit(number=int(100000 / size[i])) / int(100000 / size[i])
+    new_time = t.timeit(number=int(1e9 / size[i] ** 2)) / int(1e9 / size[i] ** 2)
 
     print("matrix size : " + str(size[i]) + "x" + str(size[i]) + "\t\told time : " + str(
         old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
