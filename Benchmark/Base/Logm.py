809f794aa05341403a724ca4413cb4b284ece9d2
DA ROCHA Romain <darocha.romain@gmail.com>
2015-12-04 10:23:07 +0100

    modif benchmark

--- Benchmark/Base/Logm.py
+++ Benchmark/Base/Logm.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".
 
 from Utils.CovMat import CovMat
 
-size = [10, 25, 50, 75, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 1000]
+size = [10, 25, 50, 75, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000]
 
 # WARMUP
 print("Warm up...")
@@ -20,11 +20,11 @@ for i in range(0, len(size)):
     covmat = numpy.dot(A, A.T)
     t = timeit.Timer("logm(covmat)",
                      setup="from __main__ import covmat; from oldPyRiemann.base import logm; import Utils.OpenBLAS")
-    old_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+    old_time = t.timeit(number=int(100000 / size[i])) / int(100000 / size[i])
 
     covmat = CovMat.random(size[i])
     t = timeit.Timer("covmat.reset_fields(); covmat.logm", setup="from __main__ import covmat")
-    new_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+    new_time = t.timeit(number=int(100000 / size[i])) / int(100000 / size[i])
 
     print("matrix size : " + str(size[i]) + "x" + str(size[i]) + "\t\told time : " + str(
         old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
