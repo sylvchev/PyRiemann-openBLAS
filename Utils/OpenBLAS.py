e42d65fb3664ef61e6ccf492c84041a091285097
DA ROCHA Romain <darocha.romain@gmail.com>
2015-12-04 10:08:00 +0100

    gestion erreur openBLAS.py

--- Utils/OpenBLAS.py
+++ Utils/OpenBLAS.py
@@ -10,31 +10,33 @@ import ctypes
 import os
 from ctypes.util import find_library
 
-openblas_lib = ctypes.cdll.LoadLibrary(find_library('openblas'))
+try:
+    openblas_lib = ctypes.cdll.LoadLibrary(find_library('openblas'))
 
 
-def get_nb_threads():
-    return openblas_lib.openblas_get_num_threads()
+    def get_nb_threads():
+        return openblas_lib.openblas_get_num_threads()
 
 
-def set_nb_threads(n):
-    openblas_lib.openblas_set_num_threads(n)
+    def set_nb_threads(n):
+        openblas_lib.openblas_set_num_threads(n)
 
 
-def get_nb_procs():
-    return openblas_lib.openblas_get_num_procs()
+    def get_nb_procs():
+        return openblas_lib.openblas_get_num_procs()
 
 
-class NbThreads(object):
-    def __init__(self, nb_threads):
-        self.old_nb_threads = get_nb_threads()
-        self.nb_threads = nb_threads
+    class NbThreads(object):
+        def __init__(self, nb_threads):
+            self.old_nb_threads = get_nb_threads()
+            self.nb_threads = nb_threads
 
-    def __enter__(self):
-        set_nb_threads(self.nb_threads)
+        def __enter__(self):
+            set_nb_threads(self.nb_threads)
 
-    def __exit__(self, *args):
-        set_nb_threads(self.old_nb_threads)
+        def __exit__(self, *args):
+            set_nb_threads(self.old_nb_threads)
 
-
-set_nb_threads(int(os.popen("cat /proc/cpuinfo | grep \"cpu cores\" | sed '1!d' | tail -c 2", "r").readline()))
+        set_nb_threads(int(os.popen("cat /proc/cpuinfo | grep \"cpu cores\" | sed '1!d' | tail -c 2", "r").readline()))
+except FileNotFoundError:
+    print("Something went wrond while loading functions in libopenblas.so...")
