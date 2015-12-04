45003bd85644371974b2cb98b821a0e4a0274681
DA ROCHA Romain <darocha.romain@gmail.com>
2015-12-04 09:58:32 +0100

    Changement de modele de benchmark

---
+++ Benchmark/Base/Invsqrtm.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import timeit
+import numpy
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from Utils.CovMat import CovMat
+
+size = [10, 25, 50, 75, 100, 250, 500, 750, 1000]
+
+# WARMUP
+print("Warm up...")
+for i in range(0, 10):
+    warm_up_covmat = CovMat.random(1000)
+    warm_up_covmat.invsqrtm
+
+for i in range(0, len(size)):
+    A = numpy.random.rand(size[i], 2 * size[i])
+    covmat = numpy.dot(A, A.T)
+    t = timeit.Timer("invsqrtm(covmat)",
+                     setup="from __main__ import covmat; from oldPyRiemann.base import invsqrtm; import Utils.OpenBLAS")
+    old_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+
+    covmat = CovMat.random(size[i])
+    t = timeit.Timer("covmat.reset_fields(); covmat.invsqrtm", setup="from __main__ import covmat")
+    new_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+
+    print("matrix size : " + str(size[i]) + "x" + str(size[i]) + "\t\told time : " + str(
+        old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
+        old_time / new_time))
---
+++ Benchmark/Base/Sqrtm.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import timeit
+import numpy
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+from Utils.CovMat import CovMat
+
+size = [10, 25, 50, 75, 100, 250, 500, 750, 1000]
+
+# WARMUP
+print("Warm up...")
+for i in range(0, 10):
+    warm_up_covmat = CovMat.random(1000)
+    warm_up_covmat.sqrtm
+
+for i in range(0, len(size)):
+    A = numpy.random.rand(size[i], 2 * size[i])
+    covmat = numpy.dot(A, A.T)
+    t = timeit.Timer("sqrtm(covmat)",
+                     setup="from __main__ import covmat; from oldPyRiemann.base import sqrtm; import Utils.OpenBLAS")
+    old_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+
+    covmat = CovMat.random(size[i])
+    t = timeit.Timer("covmat.reset_fields(); covmat.sqrtm", setup="from __main__ import covmat")
+    new_time = t.timeit(number=int(10000 / size[i])) / int(10000 / size[i])
+
+    print("matrix size : " + str(size[i]) + "x" + str(size[i]) + "\t\told time : " + str(
+        old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
+        old_time / new_time))
