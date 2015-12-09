5c7cfe72460b4a06a1f11cf29b1829fefa90e12f
21105540 <21105540@isty.uvsq.fr>
2015-12-10 00:09:02 +0100

    test unitaires supplémentaires * Ajout de Invsqrtm, Logm, Powm et Sqrtm sur la base de Expm * Import dans Test.py

--- UnitaryTests/Base/Powm.py
+++ UnitaryTests/Base/Powm.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 
@@ -6,14 +7,15 @@ import numpy
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from Utils.CovMat import CovMat
-from oldPyRiemann.base import expm
+from oldPyRiemann.base import powm
 
 
-def test_expm():
+def test_powm():
     numpy_array = numpy.array([[2, 1, 0], [1, 2, 0], [0, 0, 3]])
-    if (CovMat(numpy_array).expm - CovMat(expm(numpy_array))).norm < 1e-10:
-        print("expm: PASS")
+    if (CovMat(numpy_array).powm(5) - CovMat(powm(numpy_array, 5))).norm < 1e-10:
+        print("powm: PASS")
         return True
     else:
-        print("expm: FAIL")
+        print("powm: FAIL")
         return False
+
--- UnitaryTests/Test.py
+++ UnitaryTests/Test.py
@@ -3,9 +3,33 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from UnitaryTests.Base.Expm import test_expm
+# test cases (nose)
 
+# Base test cases
+
+from UnitaryTests.Base.Expm import test_expm
+from UnitaryTests.Base.Sqrtm import test_sqrtm
+from UnitaryTests.Base.Logm import test_logm
+from UnitaryTests.Base.Powm import test_powm
+from UnitaryTests.Base.Invsqrtm import test_invsqrtm
+
+# Distance test cases
+
+
+
+# Geodesic test cases
+
+
+
+# end of test cases
+"""
 if test_expm():
     print("Test expm : Passed")
 else:
     print("Test expm : Failed")
+"""
+
+
+
+
+
