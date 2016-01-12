9f0e3ec768848670343aa5473c4ae6b332f836af
DA ROCHA Romain <darocha.romain@gmail.com>
2016-01-12 10:05:44 +0100

    test

--- Benchmark/Custom/RiemannianDataAug.py
+++ Benchmark/Custom/RiemannianDataAug.py
@@ -12,14 +12,17 @@ for i in range(0, 10):
     warm_up_covmat = CovMat.random(1000)
     warm_up_covmat.expm
 
-t = timeit.Timer("compute(100, 10)",
-                 setup="from Benchmark.Custom.RiemannianDataAug.RiemannianDataAugOld import compute")
-old_time = t.timeit(number=2) / 2
+size = [10, 25, 50, 75, 100, 250, 500]
 
-t = timeit.Timer("compute(100, 10)",
-                 setup="from Benchmark.Custom.RiemannianDataAug.RiemannianDataAugNew import compute")
-new_time = t.timeit(number=2) / 2
+for i in range(0, len(size)):
+    t = timeit.Timer("compute(100, 10)",
+                     setup="from Benchmark.Custom.RiemannianDataAug.RiemannianDataAugOld import compute")
+    old_time = t.timeit(number=2) / 2
 
-print("matrix size : " + str(100) + "x" + str(100) + "\t\told time : " + str(
-    old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
-    old_time / new_time))
+    t = timeit.Timer("compute(100, 10)",
+                     setup="from Benchmark.Custom.RiemannianDataAug.RiemannianDataAugNew import compute")
+    new_time = t.timeit(number=2) / 2
+
+    print("matrix size : " + str(size[i]) + "x" + str(size[i]) + "\t\told time : " + str(
+        old_time) + " sec\t\t" + "new time : " + str(new_time) + " sec\t\t" + "speed up : " + str(
+        old_time / new_time))
