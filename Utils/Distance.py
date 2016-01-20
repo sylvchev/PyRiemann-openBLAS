e60c081168fe92d770aedcc5cc67acab482c5f53
DaRochaRomain <darocha.romain@gmail.com>
2016-01-20 18:37:56 +0100

    Update Distance.py

--- Utils/Distance.py
+++ Utils/Distance.py
@@ -16,7 +16,6 @@ class Distance(object):
 
     @staticmethod
     def log_euclidean(covmat1, covmat2):
-        print(covmat1.logm - covmat2.logm)
         return (covmat1.logm - covmat2.logm).norm(ord='fro')
 
     @staticmethod
